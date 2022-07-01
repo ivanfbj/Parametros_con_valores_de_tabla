d22324833e690b9e51e2130d3dbe21b068569c06
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2022-07-01 07:31:27 -0500

    Insert with table-valued-parameters

---
+++ ClsConexionSql.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AplicacionConsola
+{
+    class ClsConexionSql
+    {
+
+        static private string cadenaConexion = "server=AGN5\\SQL ; database = local_test; integrated security = true ";
+
+        private SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AplicacionConsola
+{
+
+    /*
+         CREATE TABLE [dbo].[tblNames](
+	    [FirstName] [nvarchar](100) NULL,
+	    [LastName] [nvarchar](100) NULL
+        ) ON [PRIMARY]
+        GO
+        CREATE TYPE [dbo].[tptblNames] AS TABLE(
+	    [FirstName] [nvarchar](100) NULL,
+	    [LastName] [nvarchar](100) NULL
+        )
+        GO
+        CREATE PROCEDURE [dbo].[prInsertNames] (
+        @Names tptblNames READONLY -- Nota: Debe especificar READONLY
+            )
+            AS
+            BEGIN
+	            INSERT INTO dbo.TblNames (FirstName, LastName)
+		            SELECT FirstName, LastName
+		            FROM @Names;
+
+	            SELECT * FROM TblNames;
+            END
+                 GO
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataTable dtNames = new DataTable();
+
+            dtNames.Columns.Add("FirstName", typeof(string));
+            dtNames.Columns.Add("LastName", typeof(string));
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                DataRow drName = dtNames.NewRow();
+                drName["FirstName"] = $"fernando - {i}";
+                drName["LastName"] = $"jaramillo - {i}";
+
+                dtNames.Rows.Add(drName);
+            }
+            //Console.WriteLine(dtNames.Rows[0]["FirstName"]);
+            //Console.WriteLine(dtNames.Rows[0]["LastName"]);
+
+            try
+            {
+                ClsConexionSql conecctionTest = new ClsConexionSql();
+
+                // Configure the SqlCommand and table-valued parameter.  
+                SqlCommand insertCommand = new SqlCommand("prInsertNames", conecctionTest.AbrirConexion());
+                insertCommand.CommandType = CommandType.StoredProcedure;
+                SqlParameter tvpParam = insertCommand.Parameters.AddWithValue("@Names", dtNames);
+                tvpParam.SqlDbType = SqlDbType.Structured;
+
+                // Execute the command.  
+                int ejecucion = insertCommand.ExecuteNonQuery();
+
+                Console.WriteLine("Cantidad registros insertados: " + ejecucion);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
